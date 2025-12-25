183d58631b8671ed36150c0b91d5e575a25bf2ce
Andrew <38564365+fastb1t@users.noreply.github.com>
2025-12-25 13:03:08 +0200

    Update main.cpp

--- Solution/timestamp-modifier/main.cpp
+++ Solution/timestamp-modifier/main.cpp
@@ -1,28 +1,36 @@
-#include <windows.h>
+#include <string>
 #include <iostream>
+#include <windows.h>
 
 FILETIME MakeFileTime(int year, int month, int day, int hour, int minute, int second)
 {
-    SYSTEMTIME st = {};
-    st.wYear = year;
-    st.wMonth = month;
-    st.wDay = day;
-    st.wHour = hour;
-    st.wMinute = minute;
-    st.wSecond = second;
+    SYSTEMTIME st;
+    memset(&st, 0, sizeof(SYSTEMTIME));
+
+    st.wYear    = year;
+    st.wMonth   = month;
+    st.wDay     = day;
+    st.wHour    = hour;
+    st.wMinute  = minute;
+    st.wSecond  = second;
 
     FILETIME ftLocal, ftUTC;
+    memset(&ftLocal, 0, sizeof(FILETIME));
+    memset(&ftUTC, 0, sizeof(FILETIME));
+
     SystemTimeToFileTime(&st, &ftLocal);
     LocalFileTimeToFileTime(&ftLocal, &ftUTC);
+    
     return ftUTC;
 }
 
-int main()
+// Entry point.
+int main(int argc, char* argv[])
 {
-    const char* filename = "test.txt";
+    const std::string filename = "test.txt";
 
     HANDLE hFile = CreateFileA(
-        filename,
+        filename.c_str(),
         FILE_WRITE_ATTRIBUTES,
         FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
         NULL,
@@ -32,22 +40,26 @@ int main()
     );
 
     if (hFile == INVALID_HANDLE_VALUE) {
-        std::cerr << "Не можу відкрити файл\n";
+        DWORD dwLastError = GetLastError();
+        std::cerr << "Error! CreateFile() failed with error code: " << dwLastError << "\n";
         return 1;
     }
 
-    // Встановимо новий час
+
     FILETIME creationTime = MakeFileTime(2023, 1, 1, 12, 0, 0);
     FILETIME lastAccessTime = MakeFileTime(2023, 1, 2, 13, 30, 0);
     FILETIME lastWriteTime = MakeFileTime(2023, 1, 3, 15, 45, 0);
 
     if (!SetFileTime(hFile, &creationTime, &lastAccessTime, &lastWriteTime)) {
-        std::cerr << "Помилка SetFileTime\n";
+        DWORD dwLastError = GetLastError();
+        std::cerr << "Error! SetFileTime() failed with error code: " << dwLastError << "\n";
         CloseHandle(hFile);
         return 2;
     }
 
     CloseHandle(hFile);
-    std::cout << "Дати успішно змінені!\n";
-    return 0;
+    
+    std::cout << "Done!\n";
+
+    return EXIT_SUCCESS;
 }
